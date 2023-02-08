2a466ff01fd6e1e3912f031aefd343fa2fcddc8d
imperio0001 <imperio_000@hotmail.com>
2023-02-08 11:22:15 -0300

    Insere projeto inicial

---
+++ src/main/java/com/projetointegradorODS1/ProjetoIntegradorOds1Application.java
@@ -0,0 +1,13 @@
+package com.projetointegradorODS1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoIntegradorOds1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoIntegradorOds1Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/projetointegradorODS1/model/Tema.java
@@ -0,0 +1,4 @@
+package com.projetointegradorODS1.model;
+
+public class Tema {
+}
