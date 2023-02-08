76ddc3dda3446ef741795e6c0f6e58a1a00340b7
imperio0001 <imperio_000@hotmail.com>
2023-02-08 12:27:16 -0300

    adiciona classe Postagem e Tema

--- src/main/java/com/projetointegradorODS1/model/Postagem.java
+++ src/main/java/com/projetointegradorODS1/model/Postagem.java
@@ -1,4 +1,94 @@
 package com.projetointegradorODS1.model;
 
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tb_postagens")
 public class Postagem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "O atributo título é Obrigatório!")
+    @Size(min = 5, max = 100, message = "O atributo título deve conter no mínimo 05 e no máximo 100 caracteres")
+    private String titulo;
+
+    @NotBlank(message = "O atributo mensagem é Obrigatório!")
+    @Size(min = 10, max = 1000, message = "O atributo mensagem deve conter no mínimo 10 e no máximo 1000 caracteres")
+    private String mensagem;
+
+    @UpdateTimestamp
+    private LocalDateTime data;
+
+    @NotBlank(message = "O atributo compartilhamento é Obrigatório!")
+    private Boolean compartilhamento;
+
+    @NotBlank(message = "O atributo aceita_comentario é Obrigatório!")
+    private Boolean aceita_comentario;
+
+    @ManyToOne(cascade = CascadeType.REMOVE, optional = false)
+    @JoinColumn(name = "tema_id", nullable = false)
+    private Tema tema;
+
+    public Tema getTema() {
+        return tema;
+    }
+
+    public void setTema(Tema tema) {
+        this.tema = tema;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+
+    public LocalDateTime getData() {
+        return data;
+    }
+
+    public void setData(LocalDateTime data) {
+        this.data = data;
+    }
+
+    public Boolean getCompartilhamento() {
+        return compartilhamento;
+    }
+
+    public void setCompartilhamento(Boolean compartilhamento) {
+        this.compartilhamento = compartilhamento;
+    }
+
+    public Boolean getAceita_comentario() {
+        return aceita_comentario;
+    }
+
+    public void setAceita_comentario(Boolean aceita_comentario) {
+        this.aceita_comentario = aceita_comentario;
+    }
 }
--- src/main/java/com/projetointegradorODS1/model/Tema.java
+++ src/main/java/com/projetointegradorODS1/model/Tema.java
@@ -1,4 +1,61 @@
 package com.projetointegradorODS1.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.List;
+
+@Entity
+@Table(name = "tb_tema")
 public class Tema {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "O atributo nome é Obrigatório!")
+    @Size(min = 5, max = 100, message = "O atributo nome deve conter no mínimo 05 e no máximo 100 caracteres")
+    private String nome;
+
+    @NotBlank(message = "O atributo descrição é Obrigatório!")
+    @Size(min = 10, max = 1000, message = "O atributo texto deve conter no mínimo 10 e no máximo 1000 caracteres")
+    private String descricao;
+
+    @OneToMany(mappedBy = "tema", cascade = CascadeType.ALL)
+    @JsonIgnoreProperties("tema")
+    private List<Postagem> postagem;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public List<Postagem> getPostagem() {
+        return postagem;
+    }
+
+    public void setPostagem(List<Postagem> postagem) {
+        this.postagem = postagem;
+    }
 }
